6f91405a726074e89490c7c065bc891152016ba2
Noah <noah.leu99@gmail.com>
2024-05-06 13:53:18 +0200

    change model 2 to linear activation

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ def main():
 
   model2 = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(64, activation='linear'),
     tf.keras.layers.Dense(10, activation='softmax')
   ])
 
