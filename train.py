df8cf271e04d69092d9d586930ca5634d66d44c3
Noah <noah.leu99@gmail.com>
2024-05-06 10:22:38 +0200

    parallel training and eval, working with mlflow

--- train.py
+++ train.py
@@ -8,147 +8,144 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import pandas as pd
-import multiprocessing as mp
 import json
+from train_parallel import main as train_parallel
 
-print(
-  "\033[35m",
-  r"""
 
-  /\ "-./  \   /\ \       /\  __ \   /\  == \ /\  ___\
-  \ \ \-./\ \  \ \ \____  \ \ \/\ \  \ \  _-/ \ \___  \
-  \ \_\ \ \_\  \ \_____\  \ \_____\  \ \_\    \/\_____\
-    \/_/  \/_/   \/_____/   \/_____/   \/_/     \/_____/
-  ________________________________________________________
-  --------------------------------------------------------
-  """,
-  "\033[0m")
+def main():
+  print(
+    "\033[35m",
+    r"""
 
-print("\033[35m", "Starting training...", "\033[0m")
+    /\ "-./  \   /\ \       /\  __ \   /\  == \ /\  ___\
+    \ \ \-./\ \  \ \ \____  \ \ \/\ \  \ \  _-/ \ \___  \
+    \ \_\ \ \_\  \ \_____\  \ \_____\  \ \_\    \/\_____\
+      \/_/  \/_/   \/_____/   \/_____/   \/_/     \/_____/
+    ________________________________________________________
+    --------------------------------------------------------
+    """,
+    "\033[0m")
 
-dagshub.auth.add_app_token(os.environ["MLFLOW_TRACKING_PASSWORD"])
-dagshub.init("mlops-presentation", "NoahLeu", mlflow=True)
+  print("\033[35m", "Starting training...", "\033[0m")
 
-mlflow.start_run()
+  dagshub.auth.add_app_token(os.environ["MLFLOW_TRACKING_PASSWORD"])
+  dagshub.init("mlops-presentation", "NoahLeu", mlflow=True)
 
-# Activate autologging with MLflow
-mlflow.tensorflow.autolog()
+  mlflow.start_run()
 
-print("\033[35m", "Loading data...", "\033[0m")
+  # Activate autologging with MLflow
+  # mlflow.tensorflow.autolog()
+  # we can't use autologging because we need to train in parallel
 
-# Load MNIST data
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+  print("\033[35m", "Loading data...", "\033[0m")
 
-# Split data in half for faster training
-x_train, _, y_train, _ = train_test_split(x_train, y_train, test_size=0.5, random_state=42)
+  # Load MNIST data
+  mnist = tf.keras.datasets.mnist
+  (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Split the training data for validation
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+  # Split data in half for faster training
+  x_train, _, y_train, _ = train_test_split(x_train, y_train, test_size=0.5, random_state=42)
 
-print("\033[35m", "Building model...", "\033[0m")
+  # Split the training data for validation
+  x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
 
-# Define model (very simple minimal example)
-model1 = tf.keras.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(10, activation='softmax')
-])
+  print("\033[35m", "Building model...", "\033[0m")
 
-model2 = tf.keras.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(64, activation='relu'),
-  tf.keras.layers.Dense(10, activation='softmax')
-])
+  # Define model (very simple minimal example)
+  model1 = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+  ])
 
-# Compile model
-model1.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-model2.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+  model2 = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+  ])
 
-val_loss1, val_acc1 = 0, 0
-val_loss2, val_acc2 = 0, 0
+  # Compile model
+  model1.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+  model2.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
 
-with mlflow.start_run(nested=True, run_name="model_1") as child_run_1:
-  print ("\033[35m", "Training model 1...", "\033[0m")
-  model1.fit(x_train, y_train, epochs=3, validation_data=(x_val, y_val))
-  print ("\033[35m", "Evaluating model 1...", "\033[0m")
-  val_loss1, val_acc1 = model1.evaluate(x_val, y_val)
+  models = [model1, model2]
 
-run_info = mlflow.get_run(child_run_1.info.run_id)
-assert run_info.info.status == "FINISHED"
+  metrics = train_parallel(models, x_train, y_train, x_val, y_val)
 
-with mlflow.start_run(nested=True, run_name="model_2") as child_run_2:
-  print ("\033[35m", "Training model 2...", "\033[0m")
-  model2.fit(x_train, y_train, epochs=3, validation_data=(x_val, y_val))
-  print ("\033[35m", "Evaluating model 2...", "\033[0m")
-  val_loss2, val_acc2 = model2.evaluate(x_val, y_val)
+  val_acc1, val_loss1, val_acc2, val_loss2 = 0, 0, 0, 0
+  for i, metric in enumerate(metrics):
+    if i == 0:
+      val_acc1, val_loss1 = metric['accuracy'], metric['loss']
+    else:
+      val_acc2, val_loss2 = metric['accuracy'], metric['loss']
 
-run_info = mlflow.get_run(child_run_2.info.run_id)
-assert run_info.info.status == "FINISHED"
+  print("\033[35m", "Saving metrics...", "\033[0m")
 
-print("\033[35m", "Saving metrics...", "\033[0m")
-
-# Save metrics to a CSV file
-metrics = pd.DataFrame({
+  # Save metrics to a CSV file
+  metrics = pd.DataFrame({
     'model': ['model1', 'model2'],
     'val_loss': [val_loss1, val_loss2],
     'val_acc': [val_acc1, val_acc2]
-})
+  })
 
-metrics.to_csv('metrics/metrics.csv', index=False)
+  metrics.to_csv('metrics/metrics.csv', index=False)
 
-# Select the best model
-best_model = model1 if val_acc1 > val_acc2 else model2
-best_model_name = "model1" if val_acc1 > val_acc2 else "model2"
+  # Select the best model
+  best_model = model1 if val_acc1 > val_acc2 else model2
+  best_model_name = "model1" if val_acc1 > val_acc2 else "model2"
 
-print("\033[35m", "Best model: " + best_model_name, "\033[0m")
+  print("\033[35m", "Best model: " + best_model_name, "\033[0m")
 
-print("\033[35m", "Saving trained model...", "\033[0m")
+  print("\033[35m", "Saving trained model...", "\033[0m")
 
-# Saving
-best_model.save("model.h5")
+  # Saving
+  best_model.save("model.h5")
 
-print("\033[35m", "Tracking model with DVC...", "\033[0m")
+  print("\033[35m", "Tracking model with DVC...", "\033[0m")
 
-print("\033[35m", "Logging metrics and evaluating model...", "\033[0m")
+  print("\033[35m", "Logging metrics and evaluating model...", "\033[0m")
 
-# Log metrics
-pred_y = best_model.predict(x_test)
-pred_df = pd.DataFrame(pred_y, columns=[i for i in range(10)])
-pred_df['Pred'] = pred_df.idxmax(axis=1)
-pred_df['GT'] = y_test
+  # Log metrics
+  pred_y = best_model.predict(x_test)
+  pred_df = pd.DataFrame(pred_y, columns=[i for i in range(10)])
+  pred_df['Pred'] = pred_df.idxmax(axis=1)
+  pred_df['GT'] = y_test
 
-print("\033[35m", "Creating and saving confusion matrix...", "\033[0m")
+  print("\033[35m", "Creating and saving confusion matrix...", "\033[0m")
 
-# Confusion matrix plot
-cm = confusion_matrix(pred_df['GT'], pred_df['Pred'])
-cm_map = ConfusionMatrixDisplay(confusion_matrix=cm)
-cm_map.plot()
-plt.savefig("metrics/confusion_matrix.png")
+  # Confusion matrix plot
+  cm = confusion_matrix(pred_df['GT'], pred_df['Pred'])
+  cm_map = ConfusionMatrixDisplay(confusion_matrix=cm)
+  cm_map.plot()
+  plt.savefig("metrics/confusion_matrix.png")
 
-# Log confusion matrix
-mlflow.log_artifact(
-  "metrics/confusion_matrix.png",
-  "confusion_matrix.png"
-)
+  # Log confusion matrix
+  mlflow.log_artifact(
+    "metrics/confusion_matrix.png",
+    "confusion_matrix.png"
+  )
 
-# Log metrics metrics.json
-metrics = {
+  # Log metrics metrics.json
+  metrics = {
     "accuracy": val_acc1,
     "loss": val_loss1
-}
+  }
 
-with open("metrics/metrics.json", "w") as f:
-    json.dump(metrics, f)
+  with open("metrics/metrics.json", "w") as f:
+      json.dump(metrics, f)
 
-print("\033[35m", "Logging model to MLflow...", "\033[0m")
-# Save model in MLflow format
-mlflow.tensorflow.log_model(best_model, "model")
+  print("\033[35m", "Logging model to MLflow...", "\033[0m")
+  # Save model in MLflow format
+  mlflow.tensorflow.log_model(best_model, "model")
 
-mlflow.end_run()
+  mlflow.end_run()
 
-print("\033[32m", "Run finished successfully.", "\033[0m")
+  print("\033[32m", "Run finished successfully.", "\033[0m")
+
+
+if __name__ == "__main__":
+  main()
---
+++ train_parallel.py
@@ -0,0 +1,51 @@
+import mlflow
+import mlflow.tensorflow
+from return_thread import ReturnValueThread
+
+def train_and_log(model, run_name, x_train, y_train, x_val, y_val):
+  with mlflow.start_run(run_name=run_name, nested=True) as run:
+    # Train and evaluate the model
+    print ("\033[35m", f"Training {run_name}...", "\033[0m")
+    history = model.fit(x_train, y_train, epochs=3, validation_data=(x_val, y_val))
+    print ("\033[35m", f"Evaluating {run_name}...", "\033[0m")
+    val_loss, val_acc = model.evaluate(x_val, y_val)
+    # Log metrics
+    for epoch in range(len(history.history['loss'])):
+      epoch_metrics = {
+        "train_loss": history.history['loss'][epoch],
+        "train_acc": history.history['accuracy'][epoch],
+        "val_loss": history.history['val_loss'][epoch],
+        "val_acc": history.history['val_accuracy'][epoch]
+      }
+
+      mlflow.log_metrics(epoch_metrics, step=epoch)
+
+
+    final_metrics = {
+      "accuracy": val_acc,
+      "loss": val_loss
+    }
+
+    mlflow.log_metrics(final_metrics)
+
+    # Save model in MLflow format
+    # mlflow.tensorflow.log_model(model, "model")
+
+    return final_metrics
+
+def main(models, x_train, y_train, x_val, y_val):
+  threads = []
+  for i, model in enumerate(models):
+    t = ReturnValueThread(target=train_and_log, args=(model, f"model_{i+1}", x_train, y_train, x_val, y_val))
+    threads.append(t)
+
+  for t in threads:
+    t.start()
+
+  metrics = []
+  for t in threads:
+    metric_pair = t.join()
+    metrics.append(metric_pair)
+
+  # form: [{'accuracy': 0.5, 'loss': 0.5}, {'accuracy': 0.5, 'loss': 0.5}]
+  return metrics
